b2f6a50ca21bbd4330b0dc4e34676d693c539aa2
Emmanuele Bassi <ebassi@gnome.org>
2019-03-15 11:52:55 +0000

    ci: Add AppVeyor support
    This won't pass because there's still a bunch of Linux-specific code.

---
+++ .appveyor/msvc.bat
@@ -0,0 +1,38 @@
+@echo on
+
+set MESON_VERSION = "0.50.0"
+
+:: Download Meson and Ninja, create install directory
+mkdir _build
+mkdir graphene-shared-%MSVC_PLATFORM%
+cd _build
+curl -LsSO https://github.com/mesonbuild/meson/releases/download/%MESON_VERSION%/meson-%MESON_VERSION%.tar.gz
+7z x meson-%MESON_VERSION%.tar.gz
+move dist\meson-%MESON_VERSION%.tar .
+7z x meson-%MESON_VERSION%.tar
+rmdir dist
+del meson-%MESON_VERSION%.tar meson-%MESON_VERSION%.tar.gz
+curl -LsSO https://github.com/ninja-build/ninja/releases/download/v1.7.2/ninja-win.zip
+7z x ninja-win.zip
+del ninja-win.zip
+cd ..
+
+:: Build and install
+cd _build
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %MSVC_PLATFORM%
+@echo on
+C:\Python36\python.exe meson-%MESON_VERSION%\meson.py .. . --backend=ninja --prefix=%APPVEYOR_BUILD_FOLDER%\mutest-shared-%MSVC_PLATFORM% || goto :error
+ninja || goto :error
+ninja test || goto :error
+ninja install || goto :error
+cd ..
+
+:: Copy license into install directory and create .zip file
+copy LICENSE mutest-shared-%MSVC_PLATFORM% || goto :error
+dir mutest-shared-%MSVC_PLATFORM% /s /b || goto :error
+7z a -tzip mutest-shared-win-%MSVC_PLATFORM%.zip mutest-shared-%MSVC_PLATFORM% || goto :error
+
+goto :EOF
+
+:error
+exit /b 1
