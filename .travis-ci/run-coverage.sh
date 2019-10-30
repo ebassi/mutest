2ab0e3e6c44e72ec0007dfbd3f0038763c8bcc30
Emmanuele Bassi <ebassi@gnome.org>
2019-10-30 19:30:21 +0000

    ci: Add the repo token in the script

--- .travis-ci/run-coverage.sh
+++ .travis-ci/run-coverage.sh
@@ -5,6 +5,7 @@ set -x
 builddir="_build"
 srcdir=`pwd`
 
+export COVERALLS_REPO_TOKEN=YJi21gyXWjx2BsvS8HRvfwjgXTJaQTqw1
 export CFLAGS='-coverage -ftest-coverage -fprofile-arcs'
 
 meson --prefix /usr $builddir $srcdir || exit $?
@@ -12,4 +13,9 @@ meson --prefix /usr $builddir $srcdir || exit $?
 ninja -C $builddir || exit $?
 meson test -C $builddir || exit $?
 
-cpp-coveralls -r . -b $builddir -i src -i ../src -e tests -e ../tests --gcov-options='\-lp'
+cpp-coveralls \
+        -r . \
+        -b $builddir \
+        -i src -i ../src \
+        -e tests -e ../tests \
+        --gcov-options='\-lp'
