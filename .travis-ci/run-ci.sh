2de417909302ade3cda5a54b41136b379042bd96
Emmanuele Bassi <ebassi@gnome.org>
2019-10-30 19:45:26 +0000

    ci: Dump the test log
    In case of failure, we need to know what happened.

--- .travis-ci/run-ci.sh
+++ .travis-ci/run-ci.sh
@@ -8,4 +8,8 @@ srcdir=`pwd`
 meson --prefix /usr "$@" $builddir $srcdir || exit $?
 
 ninja -C $builddir || exit $?
-meson test -C $builddir || exit $?
+meson test -C $builddir || {
+        res=$?
+        cat $builddir/meson-logs/testlog.txt
+        exit $res
+}
